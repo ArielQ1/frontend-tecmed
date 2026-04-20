717b066298fa893dd994269bcff46da789527efb
ArielQ1 <aquizaya1@umsa.bo>
2026-04-20 14:52:52 -0400

    add branch Andrea

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,9 @@ export function Home () {
   return (
     <>
       <h1 className="text-3xl font-extrabold">Hola Mundp!!!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quas harum eum laudantium deserunt porro perferendis ipsum esse. Eius enim esse aliquam, aspernatur voluptatibus excepturi laborum iusto. Aut, magni asperiores?
-      Magni laudantium harum modi ullam, atque eos distinctio eveniet odio rerum, inventore vitae ratione quos, officia laborum deleniti vel quae exercitationem tenetur ipsa neque. Quibusdam modi et qui consequuntur amet.
-      Possimus, numquam quam molestiae alias voluptas corporis minus reiciendis optio architecto tenetur consequatur blanditiis doloribus et porro ea qui corrupti nemo. Eum eaque deleniti, obcaecati atque vero praesentium est assumenda?
-      Eum in expedita similique eveniet sit esse, sapiente iusto eos rem aut, reiciendis repellat saepe odit eius, quis molestias laboriosam facere non facilis doloremque nisi alias nesciunt atque! Dicta, atque!
-      Aspernatur totam error ipsum quas tenetur adipisci reiciendis ut fugit eius nemo voluptate ad esse quia, ullam labore provident, dolorum veniam sed similique? Temporibus voluptatibus cumque commodi vero harum labore!
-      Libero error assumenda pariatur et ut molestiae, velit cum necessitatibus cumque eum numquam tempore aliquid at a quis maiores rem aspernatur soluta reiciendis. Consectetur dolorum ratione facere at consequuntur maiores.
-      Officiis eaque consequuntur, modi reiciendis accusamus eum suscipit esse cumque consequatur fuga? Ad deleniti, ipsa adipisci magni consectetur autem, aperiam, temporibus reprehenderit sunt mollitia soluta ullam quia error accusantium vero?
-      Mollitia adipisci optio fuga fugit autem, inventore ratione aliquid illo placeat alias eum? Debitis error odio accusantium quidem qui sapiente. Magnam ullam molestiae suscipit veniam recusandae cumque voluptatibus officiis est?
-      Ad maiores unde, ipsam delectus eveniet at aperiam? Qui necessitatibus dolor nesciunt vel at numquam autem laboriosam dicta molestiae quis dolorum accusamus, facilis cupiditate, repellendus natus recusandae nemo, animi eveniet.
-      Harum dolorem delectus explicabo at mollitia excepturi nisi illum, ipsa ipsam placeat libero architecto nulla! Quisquam, deleniti! Nesciunt rem magni obcaecati accusamus quo. Tempora, expedita quia repellat ea qui numquam.</p>
+      <p className="text-center font-extrabold text-amber-100 bg-amber-600">
+        Andrea por si acaso esta es tu rama xD jajajaj, pucha que no hagas bien... 
+      </p>
     </>
   )
 }
